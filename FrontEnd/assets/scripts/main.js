e0e8051730733402b3dd7e82efe244f9e1492dc7
lilian <lilian.chbd@gmail.com>
2024-09-05 10:23:35 +0200

    feat: page login

--- FrontEnd/assets/scripts/main.js
+++ FrontEnd/assets/scripts/main.js
@@ -1,33 +1,74 @@
+//* AFFICHER ET FILTRER PAR CATEGORIE LES PROJETS *\\
+
 async function listsImages() {
-
-    // requête GET faite a l'API
-    const response = await fetch('http://localhost:5678/api/works'); 
-
-    // on viens recuperer les données JSON en la stockant 
+  try {
+    // récupération des projects via l'API
+    const response = await fetch("http://localhost:5678/api/works");
     const projets = await response.json();
-    
-    // affiche les projets sur la page 
-    return getProjets(projets);
+
+    const categories = await renderCategories();
+
+    renderFilterButtons(categories);
+    setupFilterButtons(projets);
+    renderProjets(projets);
+  } catch (error) {
+    console.error("Erreur lors de la récupération des données :", error);
   }
-  
+}
 
-  function getProjets(projets) {
+const renderCategories = async () => {
+  const response = await fetch("http://localhost:5678/api/categories");
 
-    const gallery = document.querySelector(".gallery");
-    gallery.innerHTML = ""; 
-  
-    projets.forEach(projet => {
+  return await response.json();
+};
 
-      const figure = document.createElement("figure");
-      const image = document.createElement("img");
-      image.src = projet.imageUrl;
-      const figcaption = document.createElement("figcaption");
-      figcaption.innerText = projet.title;
-  
-      figure.appendChild(image);
-      figure.appendChild(figcaption);
-      gallery.appendChild(figure);
+const renderProjets = (projets) => {
+  document.querySelector(".gallery").innerHTML = projets
+    .map(
+      (projet) => `
+    <figure>
+      <img src="${projet.imageUrl}" alt="${projet.title}">
+      <figcaption>${projet.title}</figcaption>
+    </figure>
+  `
+    )
+    .join("");
+};
+
+const filterProjets = (projets, category) => {
+  return category === "all"
+    ? projets
+    : projets.filter((projet) => projet.categoryId == category);
+};
+
+const renderFilterButtons = (categories) => {
+  const filterButton = document.querySelector("#filter-btn");
+  filterButton.innerHTML = "";
+
+  filterButton.innerHTML += `
+    <button data-category="all" class="active">Tous</button>
+  `;
+
+  categories.forEach((category) => {
+    filterButton.innerHTML += `
+      <button data-category="${category.id}">${category.name}</button>
+    `;
+  });
+};
+
+const setupFilterButtons = (projets) => {
+  const buttons = document.querySelectorAll("#filter-btn button");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      buttons.forEach((btn) => btn.classList.remove("active"));
+
+      button.classList.add("active");
+
+      const filteredProjets = filterProjets(projets, button.dataset.category);
+      renderProjets(filteredProjets);
     });
-  }
-  
-  listsImages();
+  });
+};
+
+listsImages();
